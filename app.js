4873c62fc815315121cac0acb29a287e29cc67bc
sabrina <sabrinaabrodi20@gmail.com>
2022-06-24 14:55:13 +0200

    adding html elements

--- app.js
+++ app.js
@@ -5,5 +5,42 @@ window.addEventListener("load", () => {
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    const task = input.value;
+
+    const taskEl = document.createElement("div");
+    taskEl.classList.add("task");
+
+    const taskContentEl = document.createElement("div");
+    taskContentEl.classList.add("content");
+
+    taskEl.appendChild(taskContentEl);
+
+    const taskInputEl = document.createElement("input");
+    taskInputEl.classList.add("text");
+    taskInputEl.type = "text";
+    taskInputEl.value = task;
+    taskInputEl.setAttribute("readonly", "readonly");
+
+    taskContentEl.appendChild(taskInputEl);
+
+    const taskActionEl = document.createElement("div");
+    taskActionEl.classList.add("actions");
+
+    const taskEditEl = document.createElement("button");
+    taskEditEl.classList.add("edit");
+    taskEditEl.innerText = "Edit";
+
+    const taskDeleteEl = document.createElement("button");
+    taskDeleteEl.classList.add("delete");
+    taskDeleteEl.innerText = "Delete";
+
+    taskActionEl.appendChild(taskEditEl);
+    taskActionEl.appendChild(taskDeleteEl);
+
+    taskEl.appendChild(taskActionEl);
+
+    listEl.appendChild(taskEl);
+
+    input.value = "";
   });
 });
