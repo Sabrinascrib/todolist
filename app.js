7b799779271621c4784f1ef389af8f393e46fb31
sabrina <sabrinaabrodi20@gmail.com>
2022-06-24 14:36:13 +0200

    reventing the page from refreshing when clicking submit

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ window.addEventListener("load", () => {
   const form = document.querySelector("#new-task-form");
   const input = document.querySelector("#new-task-input");
   const listEl = document.querySelector("#tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
 });
